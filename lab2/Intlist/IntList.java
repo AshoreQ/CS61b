91b8439f69dd09c4bd61a5cbea995318cffb8c54
AshoreQ <qiuyong.332211@qq.com>
2022-12-14 20:42:16 +0800

    modify first

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,6 +81,9 @@ public class IntList {
             */
 
     public static IntList dcatenate(IntList A, IntList B) {
+        if (A == null && B == null) {
+            return null;
+        }
         IntList p = A;
         while(p.rest != null) {
             p = p.rest;
@@ -94,6 +97,9 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
+        if(A == null && B == null) {
+            return null;
+        }
         IntList A1 = new IntList(A.first, null);
         IntList ptr1 = A;
         IntList ptr2 = A1;
