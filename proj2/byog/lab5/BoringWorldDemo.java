ff92872f7750ce40d7357218abb3408165f67281
AshoreQ <qiuyong.332211@qq.com>
2022-12-27 15:17:40 +0800

    finish proj2-phase1

--- proj2/byog/lab5/BoringWorldDemo.java
+++ proj2/byog/lab5/BoringWorldDemo.java
@@ -24,12 +24,20 @@ public class BoringWorldDemo {
             }
         }
 
+        for (int x = 0; x < 60; x += 1) {
+            for (int y = 0; y < 30; y += 1) {
+                world[x][y] = Tileset.GRASS;
+            }
+        }
+
+        /*
         // fills in a block 14 tiles wide by 4 tiles tall
         for (int x = 20; x < 35; x += 1) {
             for (int y = 5; y < 10; y += 1) {
                 world[x][y] = Tileset.WALL;
             }
         }
+         */
 
         // draws the world to the screen
         ter.renderFrame(world);
