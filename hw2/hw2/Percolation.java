21dd69a28a2e717f67b8d7617b69bb716add2539
AshoreQ <qiuyong.332211@qq.com>
2023-01-02 21:41:57 +0800

    modify hw2/Percolation.java

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -8,13 +8,13 @@ public class Percolation {
     private boolean[][] sites;
     private WeightedQuickUnionUF set;
 
-    private int xyToX(int x, int y) {
+    private int xyTo1D(int x, int y) {
         return x * n + y;
     }
 
     public Percolation(int N) {
         if (N <= 0) {
-            throw new IllegalArgumentException("N is illegal, please input again !");
+            throw new IllegalArgumentException("N < 0");
         }
 
         n = N;
@@ -22,11 +22,11 @@ public class Percolation {
         sites = new boolean[n][n];
 
         for (int i = 0; i < n; ++i) {
-            set.union(0, i + 1);
+            set.union(n * n, i);
             for (int j = 0; j < n; ++j) {
                 sites[i][j] = false;
                 if (i == n - 1) {
-                    set.union(n * n + 1, xyToX(i, j));
+                    set.union(n * n + 1, xyTo1D(i, j));
                 }
             }
         }
@@ -34,61 +34,62 @@ public class Percolation {
 
     public void open(int row, int col) {
         if (row < 0 || col < 0 || row >= n || col >= n) {
-            throw new IndexOutOfBoundsException("Please enter valid arguments !");
+            throw new IndexOutOfBoundsException("row:" + row + " col:" + col);
         }
 
         if (row >= 1 && row <= n - 2) {
             if (sites[row - 1][col]) {
-                set.union(xyToX(row, col), xyToX(row - 1, col));
+                set.union(xyTo1D(row, col), xyTo1D(row - 1, col));
             }
             if (sites[row + 1][col]) {
-                set.union(xyToX(row, col), xyToX(row + 1, col));
+                set.union(xyTo1D(row, col), xyTo1D(row + 1, col));
             }
         } else {
             if (row == 0 && sites[row + 1][col]) {
-                set.union(xyToX(row, col), xyToX(row + 1, col));
+                set.union(xyTo1D(row, col), xyTo1D(row + 1, col));
             }
             if (row == n - 1 && sites[row - 1][col]) {
-                set.union(xyToX(row, col), xyToX(row - 1, col));
+                set.union(xyTo1D(row, col), xyTo1D(row - 1, col));
             }
         }
 
         if (col >= 1 && col <= n - 2) {
             if (sites[row][col - 1]) {
-                set.union(xyToX(row, col), xyToX(row, col - 1));
+                set.union(xyTo1D(row, col), xyTo1D(row, col - 1));
             }
             if (sites[row][col + 1]) {
-                set.union(xyToX(row, col), xyToX(row, col + 1));
+                set.union(xyTo1D(row, col), xyTo1D(row, col + 1));
             }
         } else {
             if (col == 0 && sites[row][col + 1]) {
-                set.union(xyToX(row, col), xyToX(row, col + 1));
+                set.union(xyTo1D(row, col), xyTo1D(row, col + 1));
             }
             if (col == n - 1 && sites[row][col - 1]) {
-                set.union(xyToX(row, col), xyToX(row, col - 1));
+                set.union(xyTo1D(row, col), xyTo1D(row, col - 1));
             }
         }
-
-        sites[row][col] = true;
-        size += 1;
+        if (!isOpen(row, col)) {
+            sites[row][col] = true;
+            size += 1;
+        }
     }
 
     public boolean isOpen(int row, int col) {
         if (row < 0 || col < 0 || row >= n || col >= n) {
-            throw new IndexOutOfBoundsException("Please enter valid arguments !");
+            throw new IndexOutOfBoundsException("row:" + row + " col:" + col);
         }
         return sites[row][col];
     }
 
     public boolean isFull(int row, int col) {
         if (row < 0 || col < 0 || row >= n || col >= n) {
-            throw new IndexOutOfBoundsException("Please enter valid arguments !");
+            throw new IndexOutOfBoundsException("row:" + row + " col:" + col);
         }
-        int pos = xyToX(row, col);
-        if (set.find(pos) == n * n + 1 && !set.connected(pos, 0)) {
+        int pos = xyTo1D(row, col);
+        if (set.find(pos) == n * n + 1 && !set.connected(pos, n * n)) {
             return false;
         }
-        return sites[row][col] && set.connected(set.find(pos), 0);
+        return sites[row][col] && set.connected(pos, n * n);
     }
 
     public int numberOfOpenSites() {
@@ -96,7 +97,7 @@ public class Percolation {
     }
 
     public boolean percolates() {
-        return set.connected(0, n * n + 1);
+        return set.connected(n * n, n * n + 1);
     }
 
     public static void main(String[] args) {
@@ -105,7 +106,6 @@ public class Percolation {
         PercolationStats test = new PercolationStats(200, 100, pf);
         double low = test.confidenceLow(), hight = test.confidenceHigh();
         System.out.println("[" + low + ", " + hight + "]");
-        System.out.println(test.stddev());
-         */
+        System.out.println(test.stddev()); */
     }
 }
