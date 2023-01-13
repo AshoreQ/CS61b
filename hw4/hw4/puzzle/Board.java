fa055a0be243665c236a7fa075c80e5191608ce4
AshoreQ <qiuyong.332211@qq.com>
2023-01-13 13:04:03 +0800

    fix some problem

--- hw4/hw4/puzzle/Board.java
+++ hw4/hw4/puzzle/Board.java
@@ -3,81 +3,86 @@ package hw4.puzzle;
 import edu.princeton.cs.algs4.Queue;
 
 public class Board implements WorldState {
-    int[][] tiles;
-    int size;
-    final int BLANK = 0;
+    private int[][] tiles;
 
+    /**
+     * Constructs a board from an N-by-N array of tiles where
+     * tiles[i][j] = tile at row i, column j
+     */
     public Board(int[][] tiles) {
-        size = tiles.length;
-        this.tiles = new int[size][size];
-        for (int i = 0; i < size; ++i) {
-            System.arraycopy(tiles[i], 0, this.tiles[i], 0, size);
+        this.tiles = new int[tiles[0].length][tiles.length];
+        for (int i = 0; i < tiles.length; i++) {
+            System.arraycopy(tiles[i], 0, this.tiles[i], 0, tiles.length);
         }
     }
 
-    private boolean vadidate(int i, int j) {
-        if (i < 0 || i > size - 1 || j < 0 || j > size - 1) {
+    private boolean validate(int index) {
+        return index >= 0 && index < size();
+    }
+
+    /**
+     * Returns value of tile at row i, column j (or 0 if blank)
+     */
+    public int tileAt(int i, int j) {
+        if (validate(i) && validate(j)) {
+            return this.tiles[i][j];
+        } else {
             throw new java.lang.IndexOutOfBoundsException();
         }
-        return true;
-    }
-    public int tileAt(int i, int j) {
-        vadidate(i, j);
-        return this.tiles[i][j];
     }
 
+    /**
+     * Returns the board size N
+     */
     public int size() {
-        return size;
+        return this.tiles.length;
     }
 
-    @Override
     /**
      * Returns neighbors of this board.
-     * SPOILERZ: This is the answer.
+     *
+     * @source http://joshh.ug/neighbors.html
      */
+    @Override
     public Iterable<WorldState> neighbors() {
         Queue<WorldState> neighbors = new Queue<>();
-        int hug = size();
-        int bug = -1;
-        int zug = -1;
-        for (int rug = 0; rug < hug; rug++) {
-            for (int tug = 0; tug < hug; tug++) {
-                if (tileAt(rug, tug) == BLANK) {
-                    bug = rug;
-                    zug = tug;
+        int size = size();
+        int blankX = -1;
+        int blankY = -1;
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                if (tileAt(x, y) == 0) {
+                    blankX = x;
+                    blankY = y;
                 }
             }
         }
-        int[][] ili1li1 = new int[hug][hug];
-        for (int pug = 0; pug < hug; pug++) {
-            for (int yug = 0; yug < hug; yug++) {
-                ili1li1[pug][yug] = tileAt(pug, yug);
+        int[][] tile = new int[size][size];
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                tile[x][y] = tileAt(x, y);
             }
         }
-        for (int l11il = 0; l11il < hug; l11il++) {
-            for (int lil1il1 = 0; lil1il1 < hug; lil1il1++) {
-                if (Math.abs(-bug + l11il) + Math.abs(lil1il1 - zug) - 1 == 0) {
-                    ili1li1[bug][zug] = ili1li1[l11il][lil1il1];
-                    ili1li1[l11il][lil1il1] = BLANK;
-                    Board neighbor = new Board(ili1li1);
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                if (Math.abs(-blankX + x) + Math.abs(y - blankY) - 1 == 0) {
+                    tile[blankX][blankY] = tile[x][y];
+                    tile[x][y] = 0;
+                    Board neighbor = new Board(tile);
                     neighbors.enqueue(neighbor);
-                    ili1li1[l11il][lil1il1] = ili1li1[bug][zug];
-                    ili1li1[bug][zug] = BLANK;
+                    tile[x][y] = tile[blankX][blankY];
+                    tile[blankX][blankY] = 0;
                 }
             }
         }
         return neighbors;
     }
 
-    private int correctNumberAt(int x, int y) {
-        vadidate(x, y);
-        int result = x * size + y + 1;
-        if (result == size * size) {
-            result = 0;
-        }
-        return result;
-    }
+    /**
+     * Hamming estimate
+     */
     public int hamming() {
+        int size = size();
         int dist = 0;
         for (int x = 0; x < size; x++) {
             for (int y = 0; y < size; y++) {
@@ -89,20 +94,24 @@ public class Board implements WorldState {
         return dist;
     }
 
-    private int correctX(int number) {
-        if (number == 0) {
-            return size - 1;
+    private int correctNumberAt(int x, int y) {
+        if (validate(x) && validate(y)) {
+            int result = x * size() + y + 1;
+            if (result == size() * size()) {
+                result = 0;
+            }
+            return result;
+        } else {
+            throw new java.lang.IndexOutOfBoundsException();
         }
-        return (number - 1) / size;
     }
 
-    private int correctY(int number) {
-        if (number == 0) {
-            return size - 1;
-        }
-        return (number - 1) % size;
-    }
+
+    /**
+     * Manhattan estimate
+     */
     public int manhattan() {
+        int size = size();
         int dist = 0;
         for (int x = 0; x < size; x++) {
             for (int y = 0; y < size; y++) {
@@ -115,11 +124,33 @@ public class Board implements WorldState {
         return dist;
     }
 
-    @Override
+    private int correctX(int number) {
+        if (number == 0) {
+            return size() - 1;
+        }
+        return (number - 1) / size();
+    }
+
+    private int correctY(int number) {
+        if (number == 0) {
+            return size() - 1;
+        }
+        return (number - 1) % size();
+    }
+
+    /**
+     * Estimated distance to goal. This method should
+     * simply return the results of manhattan() when submitted to
+     * Gradescope.
+     */
     public int estimatedDistanceToGoal() {
         return manhattan();
     }
 
+    /**
+     * Returns true if this board's tile values are the same
+     * position as y's
+     */
     @Override
     public boolean equals(Object y) {
         if (this == y) {
@@ -128,9 +159,13 @@ public class Board implements WorldState {
         if (y == null || getClass() != y.getClass()) {
             return false;
         }
-        Board that = (Board) y;
-        for (int i = 0; i < size; ++i) {
-            for (int j = 0; j < size; ++j) {
+        Board that = ((Board) y);
+        if (size() != that.size()) {
+            return false;
+        }
+        int size = size();
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 if (tileAt(i, j) != that.tileAt(i, j)) {
                     return false;
                 }
@@ -141,15 +176,19 @@ public class Board implements WorldState {
 
     @Override
     public int hashCode() {
-        int res = 0;
-        for (int i = 0; i < size; ++i) {
-            for (int j = 0; j < size; ++j) {
-                res = res * 11 + tileAt(i, j);
+        int size = size();
+        int result = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                result = result * 11 + tileAt(i, j);
             }
         }
-        return res;
+        return result;
     }
 
+    /**
+     * Returns the string representation of the board.
+     */
     @Override
     public String toString() {
         StringBuilder s = new StringBuilder();
